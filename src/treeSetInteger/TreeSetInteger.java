c3b630bad62e8e0d8462eea610f97e229e876206
CC-Student <ovsysha16@gmail.com>
2025-04-25 14:46:05 +0200

    edit NavigableSet obj, homework

--- src/treeSetInteger/TreeSetInteger.java
+++ src/treeSetInteger/TreeSetInteger.java
@@ -8,14 +8,14 @@ public class TreeSetInteger {
   public static void main(String[] args) {
     Random r = new Random();
 
-    NavigableSet<Integer> set = new TreeSet<>((a,b) -> b - a);
+    NavigableSet<Integer> set = new TreeSet<>();
 
     for(int i = 0; i < 100; i++) {
       set.add(r.nextInt(1000));
     }
 
-    System.out.println(set);
+    System.out.println(set.descendingSet());
 
-    System.out.println(set.subSet(900, true, 800, true));
+    System.out.println(set.subSet(800, true, 900, true));
   }
 }
