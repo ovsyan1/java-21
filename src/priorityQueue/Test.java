a3c4d7c38f5eecd61d0835bc0caa0a24c7206537
Kyrylo Ovsiannik <ovsyasha16@gmail.com>
2025-04-28 16:46:26 +0200

    delete empty line, homework

--- src/priorityQueue/Test.java
+++ src/priorityQueue/Test.java
@@ -21,7 +21,6 @@ public class Test {
 
         System.out.println(priorityQueue);
 
-
         System.out.println(priorityQueue.poll());
         System.out.println(priorityQueue.poll());
         System.out.println(priorityQueue.poll());
