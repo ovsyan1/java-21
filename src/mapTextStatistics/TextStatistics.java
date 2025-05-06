31951408a5db6e5dabde1d29c4557cbdb8f22ab0
KOVS194 <ovsyasha16@gmail.com>
2025-05-06 08:20:30 +0200

    add compute and merge methods for testing them

--- src/mapTextStatistics/TextStatistics.java
+++ src/mapTextStatistics/TextStatistics.java
@@ -27,7 +27,9 @@ class TextStatistics {
         Map<Character, Integer> map = new HashMap<>();
 
         for(char ch : value.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+                map.compute(ch, (key, oldVal) -> oldVal != null ? ++oldVal : 1);
+            // map.merge(ch, 1, (oldVal, newVal) -> ++oldVal);    // { =2, a=1, s=2, D=1, t=3, u=1, e=3, g=1, H=1, i=2, n=1}
+            // map.put(ch, map.getOrDefault(ch, 0) + 1); // { =2, a=1, s=2, t=3, D=1, e=3, u=1, g=1, H=1, i=2, n=1}
         }
 
         return map;
