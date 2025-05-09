065d09ca1b7edd5fe1155060b545576dbfaa7d19
CC-Student <ovsysha16@gmail.com>
2025-05-09 16:25:05 +0200

    add ReducePersonen, homework

---
+++ src/reducePersonen/ReducePersonen.java
@@ -0,0 +1,99 @@
+package reducePersonen;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+class Person {
+  private String vorname = "";
+  private String nachname = "";
+
+  Person() {
+  }
+
+  Person(String vorname, String nachname) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+  }
+
+  String getVorname() {
+    return this.vorname;
+  }
+
+  String getNachname() {
+    return this.nachname;
+  }
+
+  void setVorname(String vorname) {
+    this.vorname = vorname;
+  }
+
+  void setNachname(String nachname) {
+    this.nachname = nachname;
+  }
+
+  @Override
+  public String toString() {
+    return this.vorname + " " + this.nachname;
+  }
+}
+
+public class ReducePersonen {
+  public static void main(String... args) {
+    List<Person> list = Arrays.asList(
+      new Person("Tom", "Patze"),
+      new Person("Jerry", "Maus"),
+      new Person("Alexander", "Poe")
+    );
+
+    BinaryOperator<Person> acc = (a, b) -> {
+      var person = new Person();
+
+      if (a.getVorname().compareTo(b.getVorname()) > 0) {
+        person.setVorname(a.getVorname());
+      } else {
+        person.setVorname(b.getVorname());
+      }
+      if (a.getNachname().compareTo(b.getNachname()) > 0) {
+        person.setNachname(a.getNachname());
+      } else {
+        person.setNachname(b.getNachname());
+      }
+
+      return person;
+    };
+
+    list.stream()
+      .reduce(acc)
+      .ifPresent(System.out::println);
+
+    BinaryOperator<Person> acc2 = (a, b) -> {
+      var person = new Person();
+
+      if (a.getVorname().isEmpty()) {
+        person.setVorname(b.getVorname());
+        if (a.getNachname().isEmpty()) {
+          person.setNachname(b.getNachname());
+        }
+      } else {
+        if (a.getVorname().compareTo(b.getVorname()) > 0) {
+          person.setVorname(a.getVorname());
+        } else {
+          person.setVorname(b.getVorname());
+        }
+        if (a.getNachname().compareTo(b.getNachname()) > 0) {
+          person.setNachname(a.getNachname());
+        } else {
+          person.setNachname(b.getNachname());
+        }
+      }
+
+      return person;
+    };
+
+    System.out.println("2: " + list.stream()
+      .reduce(new Person(), acc2));
+
+    System.out.println("3: " + list.stream().reduce(new Person(), acc2, (a, b) -> a));
+  }
+}
