db8cd861be70ca70a95e105ae372f1bab32f9621
KOVS194 <ovsyasha16@gmail.com>
2025-05-13 10:09:15 +0200

    edit CollectNumbers

--- src/collectNumbers/CollectNumbers.java
+++ src/collectNumbers/CollectNumbers.java
@@ -3,7 +3,6 @@ package collectNumbers;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.BiConsumer;
 
 public class CollectNumbers {
     public static void main(String... args) {
@@ -15,13 +14,16 @@ public class CollectNumbers {
 
         List<Integer> list;
 
-        BiConsumer<List<Integer>, List<Integer>> cmb = List::addAll;
+        //BiConsumer<List<Integer>, List<Integer>> cmb = List::addAll;
         list = Arrays.stream(arr)
-                .map((str) -> Arrays.stream(str.split(",")).map(Integer::valueOf).toList())
-                .flatMap(List:: stream)
+                //.map((str) -> Arrays.stream(str.split(",")).map(Integer::valueOf).toList())
+                //.flatMap(List:: stream)
+                .map(str -> str.split(","))
+                .flatMap(Arrays::stream)
+                .map(Integer::valueOf)
                 .filter(a -> a % 2 == 0)
                 .parallel()
-                .collect(ArrayList::new, List::add, cmb);
+                .collect(ArrayList::new, List::add, List::addAll);
 
 
         System.out.println(list); // [1, 2, 3, 4, 5, 7, 6, 5, 4, 3, 123, 456]
