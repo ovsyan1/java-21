1b7272782814a29098de65d9a86cd065f681a1ef
KOVS194 <ovsyasha16@gmail.com>
2025-05-13 11:08:15 +0200

    add to CollectNumbers collect method only with one param

--- src/collectNumbers/CollectNumbers.java
+++ src/collectNumbers/CollectNumbers.java
@@ -3,6 +3,7 @@ package collectNumbers;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class CollectNumbers {
     public static void main(String... args) {
@@ -27,5 +28,15 @@ public class CollectNumbers {
 
 
         System.out.println(list); // [1, 2, 3, 4, 5, 7, 6, 5, 4, 3, 123, 456]
+
+        list = Arrays.stream(arr)
+                .map(str -> str.split(","))
+                .flatMap(Arrays::stream)
+                .map(Integer::valueOf)
+                .filter(a -> a % 2 == 0)
+                .parallel()
+                .collect(Collectors.toList());
+
+        System.out.println(list);
     }
 }
