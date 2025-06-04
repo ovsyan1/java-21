1594698188358638bd18918c95854f6905befe08
Kyrylo Ovsiannik <ovsyasha16@gmail.com>
2025-06-04 13:54:51 +0200

    add CyclicBarrierSimple, homework

---
+++ src/threads/simpleApi/cyclicBarrierSimple/CyclicBarrierSimple.java
@@ -0,0 +1,28 @@
+package threads.simpleApi.cyclicBarrierSimple;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CyclicBarrierSimple {
+    static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String... unused) throws InterruptedException, BrokenBarrierException {
+        int barrier = 1_000_000;
+        CyclicBarrier c1 = new CyclicBarrier(2, () -> System.out.println(count.get()));
+
+        Runnable task = () -> {
+            for (int i = 0; i < barrier; i++) {
+                count.incrementAndGet();
+                try {
+                    c1.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new Thread(task).start();
+        new Thread(task).start();
+    }
+}
